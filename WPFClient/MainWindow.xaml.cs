ee44a99d3d9f4b1ebc4195d2f3c3d0342069b901
nevitia <oleg.altavista@gmail.com>
2015-10-31 02:31:53 +0300

    Изучение и комментирование

---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace WPFClient
+{   
+    public partial class MainWindow : Window
+    {
+
+        public String UserName { get; set; }
+        public IHubProxy HubProxy { get; set; }
+        const string ServerURI = "http://localhost:8080/signalr";
+        public HubConnection Connection { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent(); //инициализация всех графических компонентов конструктора форм
+        }
+
+        //Соединение с сервером: вызов асинхоронного метода ConnectAsync()
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserName = UserNameTextBox.Text;
+          
+            if (!String.IsNullOrEmpty(UserName))
+            {
+                StatusText.Visibility = Visibility.Visible;
+                StatusText.Content = "Connecting to server...";
+                ConnectAsync();
+            }
+        }
+        
+        //Создание соединения
+        private async void ConnectAsync()
+        {
+            Connection = new HubConnection(ServerURI);
+            Connection.Closed += Connection_Closed; //Метод при потери соединения
+            HubProxy = Connection.CreateHubProxy("MyHub");
+            //Обработчик получения сообщения от сервера. Запись сообщения в поток UI из потока SignalR
+            HubProxy.On<string, string>("AddMessage", (name, message) =>
+                this.Dispatcher.Invoke(() =>
+                    RichTextBoxConsole.AppendText(String.Format("{0}: {1}\r", name, message))
+                )
+            );
+            try
+            {
+                await Connection.Start();
+            }
+            catch (HttpRequestException)
+            {
+                StatusText.Content = "Unable to connect to server: Start server before connecting clients.";
+                return;
+            }
+
+            //Скрытие поля авторизации, показ поля отправки сообщения
+            SignInPanel.Visibility = Visibility.Collapsed;
+            ChatPanel.Visibility = Visibility.Visible;
+            ButtonSend.IsEnabled = true;
+            TextBoxMessage.Focus();
+            RichTextBoxConsole.AppendText("Connected to server at " + ServerURI + "\r");
+        }
+
+        //Отправка сообщения
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            HubProxy.Invoke("Send", UserName, TextBoxMessage.Text);
+            TextBoxMessage.Text = String.Empty;
+            TextBoxMessage.Focus();
+        }
+
+
+        //Действие при потере соединения с сервером
+        void Connection_Closed()
+        {
+            //Скрытие поля отправки сообщения, показ поля авторизации, вывод предупреждения
+            var dispatcher = Application.Current.Dispatcher;
+            dispatcher.Invoke(() => ChatPanel.Visibility = Visibility.Collapsed);
+            dispatcher.Invoke(() => ButtonSend.IsEnabled = false);
+            dispatcher.Invoke(() => StatusText.Content = "You have been disconnected.");
+            dispatcher.Invoke(() => SignInPanel.Visibility = Visibility.Visible);
+        }
+
+
+        //Действие при закрытии клиента
+        private void WPFClient_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Connection != null)
+            {
+                Connection.Stop();
+                Connection.Dispose();
+            }
+        }  
+    }
+}
---
+++ WPFServer/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFServer
+{
+    public partial class MainWindow : Window
+    {
+        public IDisposable SignalR { get; set; } //Интерфейс освобождения неуправляемых рессурсов
+        const string ServerURI = "http://localhost:8080";
+
+        public MainWindow()
+        {
+            InitializeComponent(); //Инициализация всех графических компонентов конструктора форм
+        }
+
+        //Старт сервера в новой задаче
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            WriteToConsole("Starting server...");
+            ButtonStart.IsEnabled = false;            
+            Task.Run(() => StartServer()); //Асинхронное выполнение еденичной задачи
+        }
+
+        //Остановка сервера
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            SignalR.Dispose(); //Освобождение неуправляемого рессурса
+            Close();
+        }
+
+
+        private void StartServer()
+        {
+            try
+            {
+                SignalR = WebApp.Start(ServerURI);
+            }
+            catch (TargetInvocationException) //Задача уже выполняется
+            {
+                WriteToConsole("A server is already running at " + ServerURI);
+                this.Dispatcher.Invoke(() => ButtonStart.IsEnabled = true); //Invoke, для выполения метода из потока UI, при нахождении в другом
+                return;
+            }
+            this.Dispatcher.Invoke(() => ButtonStop.IsEnabled = true);
+            WriteToConsole("Server started at " + ServerURI);
+        }
+
+
+        public void WriteToConsole(String message)
+        {
+            if (!(RichTextBoxConsole.CheckAccess())) //Если находимся в другом потоке, доступ через Invoke
+            {
+                this.Dispatcher.Invoke(() =>
+                    WriteToConsole(message)
+                );
+                return;
+            }
+            RichTextBoxConsole.AppendText(message + "\r");
+        }
+    }
+
+
+    //Конфигурирование сервера под спецификацию owin
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+
+    public class MyHub : Hub
+    {
+        //Передача сообщения все клиентам
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+        
+        //Оповещение об установленном соединении с клиентом
+        public override Task OnConnected()
+        {
+            //Доступ к другому потоку
+            Application.Current.Dispatcher.Invoke(() => 
+                ((MainWindow)Application.Current.MainWindow).WriteToConsole("Client connected: " + Context.ConnectionId));
+
+            return base.OnConnected();
+        }
+
+        //Оповещение о разорванном соединении
+        public override Task OnDisconnected()
+        {
+            //Доступ к другому потоку
+            Application.Current.Dispatcher.Invoke(() => 
+                ((MainWindow)Application.Current.MainWindow).WriteToConsole("Client disconnected: " + Context.ConnectionId));
+
+            return base.OnDisconnected();
+        }
+
+    }
+}
